5f927be8de101de061c63494372ac4c815de157b
Artem-ganz <ganzinaartem@gmail.com>
2022-12-09 15:08:25 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// коментар
+//ім’я та прізвище в змінній
+const FIRST_NAME = 'Artem ';
+console.log(FIRST_NAME);
+
+const LAST_NAME = 'Ganzina';
+console.log(LAST_NAME);
+
+const MY_FULL_NANE = FIRST_NAME + LAST_NAME;
+console.log(MY_FULL_NANE);
+
+let secondsInAMinute = 60;
+console.log(secondsInAMinute);
+
+let age = 30;
+console.log(age);
+
+/*
+великий
+коментар
+*/
+
+
+//не вырні імя зміних
+// const animals = ['Dog ' + 'Cat ' + 'Bird'];
+// let 300Spartans = 300;
+// let color-car = 'black';
+// let default = 0;
+// let in = 12.30
+
+
+//стилі написання імен змінних
+// 1-Типы и классы - PascslCase
+// 2-Значения известны до запуска приложения и не меняються - DB_PASSWORD
+// 3-Верблюжий-горбатый регистр - loverCamelCase
+// 4-let age
+// 5-let info123
+// 6-$size
+// 7-let_color
